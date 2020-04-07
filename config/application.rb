84043617597f1f5de00aceb748b6b23dbc34b9e9
neeraj <neeraj.kumar@jarvis.consulting>
2020-04-08 02:05:08 +0530

    issue list translation done

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module FightCorona
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.i18n.default_locale = ENV["LOCALE"]
+    config.i18n.default_locale = ENV["LOCALE"].present? ? ENV["LOCALE"] : "hn"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
