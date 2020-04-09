7c65b6ed94c2f2edd19f060695e3814d9bd29990
neeraj <neeraj.kumar@jarvis.consulting>
2020-04-09 21:13:55 +0530

    issue_sub_categories translated

--- app/models/issue_sub_category.rb
+++ app/models/issue_sub_category.rb
@@ -1,4 +1,13 @@
 class IssueSubCategory < ApplicationRecord
   belongs_to :issue_category
   has_many :issue_sub_category_translations
+
+  def name
+    if REDIS.get("LOCALE").present?
+      translation = IssueSubCategoryTranslation.where(issue_sub_category_id: id, language: REDIS.get("LOCALE"))&.first&.name
+      translation.present? ? translation : read_attribute(:name)
+    else
+      read_attribute(:name)
+    end
+  end
 end
